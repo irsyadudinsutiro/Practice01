8688b72f80c73f96a56062e2446f068ab4f7f3cd
Mohamed Irsyadudin Bin Sutiro <mohamedirsyadudin@gmail.com>
2022-11-07 15:36:33 +0800

    commit Spyder

---
+++ Spyder/integers_python.py
@@ -0,0 +1,14 @@
+
+#print integerrs 1 to 10 without a loop
+print(1,2,3,4,5,6,7,8,9,10)
+
+#print integers 1 to 10 using a for loop
+for i in range(1,11):
+    print(i)
+    
+#print integers using a a while loop
+j = 1
+while(j<=10):
+    print(j)
+    #j = j + 1
+    j += 1
